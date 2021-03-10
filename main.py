0193ad5a2c41a195e9242db3faf15cc0af1c5e59
Shark1ight <gm3294@naver.com>
2021-03-10 21:24:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import config
 
-SMT = commands.Bot(command_prefix=commands.when_mentioned_or(config.Prefix), description="관리를 하는 쉬운 방법", fetch_offline_members=True, intents=discord.Intents.all())
+SMT = commands.Bot(command_prefix=commands.when_mentioned, description="관리를 하는 쉬운 방법", fetch_offline_members=True, intents=discord.Intents.all())
 SMT.remove_command('help')
 m = ['exts.global', 'exts.manage', "jishaku"]
 
