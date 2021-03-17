29a6cbc7ef6c3a3639dc91e95b819dcd5f711765
Shark1ight <gm3294@naver.com>
2021-03-17 20:51:32 +0900

    Update miya.py

--- exts/miya.py
+++ exts/miya.py
@@ -69,13 +69,13 @@ class Ticket(commands.Cog, name="티켓 지원 시스템"):
         if str(payload.emoji) == "<:cs_leave:659355468803866624>" and payload.guild_id == 564418977627897887:
             g = self.SMT.get_guild(payload.guild_id)
             ch = g.get_channel(payload.channel_id)
-            if ch.name.startswith("티켓_"):
+            if ch.name.startswith("스레드_"):
                 member = g.get_member(int(ch.topic))
                 await ch.set_permissions(member, overwrite=None)
-                name = ch.name.replace("티켓", "종료")
+                name = ch.name.replace("스레드", "종료됨")
                 await ch.edit(name=name)
                 try:
-                    await member.send(f"<:cs_leave:659355468803866624> {member.mention} - 당신이 열었던 티켓이 닫혔어요. 다시 여시려면 `상어 티켓 [ 채널 ID ]`를 사용해주세요.\n채널 ID : {ch.id}")
+                    await member.send(f"<:cs_leave:659355468803866624> {member.mention} - 스레드가 종료되었어요. 활성화하려면 `@Project. SMT#2979 티켓 {ch.id}` 명령어를 사용해주세요.")
                 except:
                     print("Discord 개인 메시지가 차단되어 전송하지 않았습니다.")
     
@@ -87,9 +87,11 @@ class Ticket(commands.Cog, name="티켓 지원 시스템"):
         if msg.channel.id == 663806592277545005:
             await msg.delete()
             category = msg.guild.get_channel(821698815944818708)
+            role = msg.guild.get_role(798615494730055730)
             overwrites = {
                 msg.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-                msg.author: discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_channels=True, manage_roles=True)
+                msg.author: discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_channels=True, manage_roles=True),
+                role: discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_channels=True, manage_roles=True),
             }
             o = await aiosqlite.connect("SMT.db")
             c = await o.cursor()
@@ -99,23 +101,23 @@ class Ticket(commands.Cog, name="티켓 지원 시스템"):
             for i in range(len(str(rows[0][1])), 4):
                 number += "0"
             number += str(rows[0][1])
-            channel = await category.create_text_channel(name=f"티켓_{number}", overwrites=overwrites, topic=msg.author.id)
-            embed = discord.Embed(title=f"{msg.author}님의 새 문의 채널", description=f"사유 : {msg.content}\n티켓을 닫으려면 아래에 있는 반응을 누르세요.", color=0xAFFDEF, timestamp=datetime.datetime.utcnow())
+            channel = await category.create_text_channel(name=f"스레드_{number}", overwrites=overwrites, topic=msg.author.id)
+            embed = discord.Embed(title=f"{msg.author}님의 새 문의 스레드", description=f"사유 : {msg.content}\n티켓을 닫으려면 아래에 있는 반응을 누르세요.", color=0xAFFDEF, timestamp=datetime.datetime.utcnow())
             embed.set_thumbnail(url=msg.author.avatar_url_as(static_format="png", size=2048))
-            embed.set_footer(text="티켓을 통한 지원 시스템", icon_url=self.SMT.user.avatar_url)
+            embed.set_footer(text="지원 스레드", icon_url=self.SMT.user.avatar_url)
             msg = await channel.send("@here", embed=embed)
             await msg.add_reaction("<:cs_leave:659355468803866624>")
             await msg.pin()
-            await c.execute(f'UPDATE shark SET ticket = {int(rows[0][1]) + 1}')
+            await c.execute(f'UPDATE shark SET number = {int(rows[0][1]) + 1}')
             await o.commit()
             await o.close()
     
     @commands.command(name="티켓")
     async def _reopen(self, ctx, channel: discord.TextChannel):
-        if channel.name.startswith("종료_"):
+        if channel.name.startswith("종료됨_"):
             if ctx.author.id == channel.topic or ctx.author.top_role.id == 659351693481345034:
                 member = ctx.guild.get_member(int(channel.topic))
-                name = channel.name.replace("종료", "티켓")
+                name = channel.name.replace("종료됨", "스레드")
                 await channel.edit(name=name)
                 overwrite = discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_channels=True, manage_roles=True)
                 await channel.set_permissions(member, overwrite=overwrite)
@@ -123,7 +125,7 @@ class Ticket(commands.Cog, name="티켓 지원 시스템"):
                 async for msg in channel.history(limit=500):
                     if msg.author == self.SMT.user:
                         message = msg
-                await message.reply("<:cs_yes:659355468715786262> @here - 지원 티켓이 티켓 소유자 또는 서버 관리자에 의해 다시 활성화됐습니다.")
+                await message.reply("<:cs_yes:659355468715786262> @here - 스레드가 티켓 관리자에 의해 다시 활성화됐습니다.")
                 await ctx.message.add_reaction("<:cs_yes:659355468715786262>")
 
 def setup(SMT):
