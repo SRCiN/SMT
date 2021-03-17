514e7fb28e1d5bbb27de741f89ea79e4c533e1e8
SRCiN <gm3294@naver.com>
2021-03-18 07:55:43 +0900

    QWERTY

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import config
 
 SMT = commands.Bot(command_prefix=commands.when_mentioned, description="관리를 하는 쉬운 방법", fetch_offline_members=True, intents=discord.Intents.all())
 SMT.remove_command('help')
-m = ['exts.global', 'exts.cmds', "jishaku", "exts.miya", "exts.events"]
+m = ['exts.global', 'exts.cmds', "jishaku", "exts.miya", "exts.events", "exts.health"]
 
 for module in m:
     SMT.load_extension(f'{module}')
