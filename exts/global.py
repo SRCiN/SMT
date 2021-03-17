87f49a6169b5f130cfb4246b4b5f1ee310be8b42
Shark1ight <gm3294@naver.com>
2021-03-17 18:12:34 +0900

    Update global.py

--- exts/global.py
+++ exts/global.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
-import datetime
-import aiohttp
-import os
-import asyncio
+import aiosqlite
 
 class Global(commands.Cog, name="범용"):
     def __init__(self, SMT):
@@ -15,6 +12,33 @@ class Global(commands.Cog, name="범용"):
         print(self.SMT.user.id)
         print("Bot is READY.")
         await self.SMT.change_presence(status=discord.Status.idle, activity=discord.Game("너와 함께"), afk=True)
+
+    @commands.command(name="DB")
+    @commands.is_owner()
+    async def _database(self, ctx, todo, *, command):
+        o = await aiosqlite.connect("SMT.db")
+        c = await o.cursor()
+        if todo == "commit":
+            await c.execute(command)
+            await o.commit()
+            await o.close()
+            await ctx.message.add_reaction("<:cs_yes:659355468715786262>")
+        elif todo == "fetch":
+            await c.execute(command)
+            a = ""
+            rows = await c.fetchall()
+            for row in rows:
+                a += f"{row}\n"
+            if len(a) <= 2000:
+                await ctx.send(a)
+            else:
+                print(a)
+                await ctx.send(a[:2000])
+            await o.close()
+            await ctx.message.add_reaction("<:cs_yes:659355468715786262>")
+        else:
+            await o.close()
+            await ctx.message.add_reaction("<:cs_trash:659355468631769101>")
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
