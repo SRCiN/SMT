deff26ab7e21664ca85eaf3aeb914324cc807112
Shark1ight <gm3294@naver.com>
2021-03-19 07:18:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
 import config
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
-SMT = commands.Bot(command_prefix=commands.when_mentioned, description="관리를 하는 쉬운 방법", fetch_offline_members=True, intents=discord.Intents.all())
-SMT.remove_command('help')
+SMT = commands.Bot(command_prefix=commands.when_mentioned, description="Shark1ight Moderation Tools", help_command=None, chunk_guilds_at_startup=True, intents=discord.Intents.all())
 m = ['exts.global', 'exts.cmds', "jishaku", "exts.miya", "exts.events", "exts.health"]
 
 for module in m:
